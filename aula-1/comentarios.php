874d7348832fce200e79610029267f736e19fd43
Ana <106841651+souanatx@users.noreply.github.com>
2023-07-20 21:35:23 +0100

    Add files via upload

---
+++ aula-1/comentarios.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title> Comentários no PHP </title>
+</head>
+<body>
+	<?php
+	//isto é um comentário no php
+	#Isto também
+	$nome = "Ana Teixeira"; //Esta variável guarda o meu nome
+	/*
+	bloco de comentários
+	*/
+	echo $nome;
+	?>
+</body>
+</html>
+
